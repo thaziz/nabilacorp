962e2dd5454f18f858c91ae5f6b8d879777a8ac0
thaziz <taziz704@gmail.com>
2018-10-16 13:26:15 +0700

    f

--- app/Modules/Produksi/model/d_product_result.php
+++ app/Modules/Produksi/model/d_product_result.php
@@ -17,22 +17,22 @@ use App\Modules\Produksi\model\d_productresult_dt;
 use Session;
 
 class d_product_result extends Model
-{
+{  
     protected $table = 'd_productresult';
     protected $primaryKey = 'pr_id';
     const CREATED_AT = 'pr_created';
     const UPDATED_AT = 'pr_updated';
-
+    
     protected $fillable = ['pr_id','pr_comp','pr_code','pr_date','pr_note','pr_status'];
 
-    static function data($request){
+    static function data($request){  
       $from=date('Y-m-d',strtotime($request->tanggal1));
       $to=date('Y-m-d',strtotime($request->tanggal2));
       $productresult=d_product_result::whereBetween('pr_date', [$from, $to])->where('pr_comp',Session::get('user_comp'))->get();
 
-       return Datatables::of($productresult)
-                        ->editColumn('pr_date', function ($productresult) {
-                                return date('d-m-Y',strtotime($productresult->pr_date));
+       return Datatables::of($productresult)                                          
+                        ->editColumn('pr_date', function ($productresult) {                            
+                                return date('d-m-Y',strtotime($productresult->pr_date));                            
                         })
                         ->addColumn('action', function ($productresult) {
                             $disable='';
@@ -40,7 +40,7 @@ class d_product_result extends Model
                               $disable='disabled';
                             }
 
-                            $html='';
+                            $html='';  
 
                           $html.='<div class="text-center">
                           <button class="btn btn-sm btn-success" title="Detail" onclick="detail(
@@ -48,7 +48,7 @@ class d_product_result extends Model
                                                 \''.$productresult->pr_code.'\',
                                                 \''.date('d-m-Y',strtotime($productresult->pr_date)).'\',
                                                 \''.$productresult->pr_note.'\',
-                          )"><i class="fa fa-eye"></i>
+                          )"><i class="fa fa-eye"></i> 
                           </button>
                           <button class="btn btn-sm btn-warning" title="Edit" onclick="edit(
                                                 '.$productresult->pr_id.',
@@ -66,9 +66,9 @@ class d_product_result extends Model
                             return $html;
                         })
                         ->rawColumns(['action','p_status'])
-                      ->make(true);
+                      ->make(true);     
 
-
+      
     }
 
     static function editDetail($id){
@@ -80,21 +80,21 @@ class d_product_result extends Model
                   $join->on('s_comp','=','prdt_comp');
                   $join->on('s_position','=','prdt_position');
                   })
-                 ->where('prdt_productresult',$id)
-                 ->orderBy('prdt_detailid','ASC')
-                 ->get();
+                 ->where('prdt_productresult',$id)                 
+                 ->orderBy('prdt_detailid','ASC')               
+                 ->get();                 
     }
 
     static function detail($id){
           return DB::table('d_productresult_dt')->join('m_item','prdt_item','=','i_id')
-                 ->join('m_satuan','s_id','=','i_satuan')
-                 ->where('prdt_productresult',$id)
-                 ->orderBy('prdt_detailid','ASC')
-                 ->get();
+                 ->join('m_satuan','s_id','=','i_satuan')                 
+                 ->where('prdt_productresult',$id)  
+                 ->orderBy('prdt_detailid','ASC')               
+                 ->get();                  
     }
 
-    static function simpan($request){
-      return DB::transaction(function () use ($request) {
+    static function simpan($request){      
+      return DB::transaction(function () use ($request) {  
        $pr_id=d_product_result::max('pr_id')+1;
                $query = DB::select(DB::raw("SELECT MAX(RIGHT(pr_code,4)) as kode_max from d_productresult WHERE DATE_FORMAT(pr_created, '%Y-%m') = DATE_FORMAT(CURRENT_DATE(), '%Y-%m')"));
       $kd = "";
@@ -122,24 +122,34 @@ class d_product_result extends Model
                       'pr_code'=>$pr_code,
                       'pr_note'=>$request->pr_note,
                 ]);
-              for ($i=0; $i <count($request->prdt_item); $i++) {
+              for ($i=0; $i <count($request->prdt_item); $i++) {  
 
               $prdt_hpp= format::format($request->prdt_hpp[$i]);
               $prdt_qty= format::format($request->prdt_qty[$i]);
               $detailid=d_productresult_dt::where('prdt_productresult',$pr_id)->max('prdt_detailid')+1;
               $comp=$request->comp[$i];
-              $position=$request->position[$i];
+              $position=$request->position[$i]; 
                d_productresult_dt::create([
                                 'prdt_productresult'=>$pr_id,
                                 'prdt_detailid'=>$detailid,
                                 'prdt_comp'=>Session::get('user_comp'),
                                 'prdt_item'=>$request->prdt_item[$i],
-                                'prdt_qty'=>$prdt_qty,
+                                'prdt_qty'=>$prdt_qty,  
+                                'prdt_qty_sisa'=>$prdt_qty,
                                 'prdt_hpp'=>$prdt_hpp,
                                 'prdt_comp'=>$comp,
                                 'prdt_position'=>$position,
                                  ]);
-                mutasi::tambahmutasi($request->prdt_item[$i],$prdt_qty,$comp,$position,'Hasil Produksi',3,$pr_id,'','',$prdt_hpp);
+                $simpanMutasi=mutasi::tambahmutasi($request->prdt_item[$i],$prdt_qty,$comp,$position,'Hasil Produksi',3,$pr_id,'','',$prdt_hpp);
+                if($simpanMutasi['true']){
+
+                }else{
+
+                  DB::rollBack();
+                  $data=['status'=>'gagal','data'=>'gagal'];
+                  return json_encode($data);
+
+                }
              }
 
           $data=['status'=>'sukses','data'=>'sukses'];
@@ -147,10 +157,10 @@ class d_product_result extends Model
 
       });
     }
+   
 
-
- static function perbarui($id,$request){
-    return DB::transaction(function () use ($id,$request) {
+ static function perbarui($id,$request){   
+    return DB::transaction(function () use ($id,$request) {  
               $updateProductresult=
                           d_product_result::where('pr_id',$id);
               $updateProductresult->update([
@@ -167,18 +177,39 @@ class d_product_result extends Model
           }
 
 
-  //Hapus Material
-        for ($h=0; $h <count($hapusdtHasil) ; $h++) {
+  //Hapus Material          
+        for ($h=0; $h <count($hapusdtHasil) ; $h++) { 
                 $hapusItem=$hapusdtHasil[$h];
                 $hapus_product_dt=d_productresult_dt::where('prdt_productresult',$id)->where('prdt_item',$hapusItem);
 
                 if(count($hapus_product_dt->first())!=0){
                   $permintaan=$hapus_product_dt->first()->prdt_qty;
 
-                  if($permintaan>0){
-                        if(mutasi::hapusMutasi($hapusItem,$permintaan,$comp=1,$position=1,$flag='Hasil Produksi',$sm_reff=$id)){
-                          $hapus_product_dt->delete();
-                        }
+                  if($permintaan>0){      
+
+              $comp=$request->comp[$h];
+              $position=$request->position[$h];                                                 
+        $simpanMutasi=mutasi::hapusMutasi($hapusItem,$permintaan,$comp,$position,$flag='Hasil Produksi',$sm_reff=$id);
+
+              if($simpanMutasi['true']){
+                    $hapus_product_dt->delete();
+              }else{
+
+                  DB::rollBack();
+                  $data=['status'=>'gagal','data'=>'gagal'];
+                  return json_encode($data);
+
+              }
+
+
+
+
+
+
+
+
+
+
                     }
                 }
           }
@@ -190,36 +221,48 @@ class d_product_result extends Model
 
 
 
-             for ($i=0; $i <count($request->prdt_item); $i++) {
+             for ($i=0; $i <count($request->prdt_item); $i++) {  
 
 
               if($request->prdt_productresult[$i]!=='' && $request->prdt_detailid[$i]!=='' &&
-                $request->prdt_productresult[$i]!==null && $request->prdt_detailid[$i]!==null){
+                $request->prdt_productresult[$i]!==null && $request->prdt_detailid[$i]!==null){  
 
-              $permintaan=format::format($request->prdt_qty[$i])-format::format($request->jumlahAwal[$i]);
+              $permintaan=format::format($request->prdt_qty[$i])-format::format($request->jumlahAwal[$i]);                
               $prdt_hpp= format::format($request->prdt_hpp[$i]);
               $comp=$request->comp[$i];
-              $position=$request->position[$i];
-              if(mutasi::perbaruimutasi($request->prdt_item[$i],$permintaan,$comp,$position,$flag='Hasil Produksi',$idFlag=1,$sm_reff=$id,$flagTujuan='',$idMutasiTujuan='',$prdt_hpp)){
+              $position=$request->position[$i]; 
+              $simpanMutasi=mutasi::perbaruimutasi($request->prdt_item[$i],$permintaan,$comp,$position,$flag='Hasil Produksi',$idFlag=3,$sm_reff=$id,$flagTujuan='',$idMutasiTujuan='',$prdt_hpp);
+              
 
-              $prdt_qty= format::format($request->prdt_qty[$i]);
+              if($simpanMutasi['true']){
+                    
+                  $prdt_qty= format::format($request->prdt_qty[$i]);
 
-              $productresult_dt=d_productresult_dt::where('prdt_productresult',$id)->where('prdt_detailid',$request->prdt_detailid[$i]);
+                  $productresult_dt=d_productresult_dt::where('prdt_productresult',$id)->where('prdt_detailid',$request->prdt_detailid[$i]);
+              
+                   $productresult_dt->update([                                
+                                    'prdt_qty'=>$prdt_qty,  
+                                    'prdt_qty_sisa'=>$prdt_qty,
+                                    'prdt_hpp'=>$prdt_hpp
+                                     ]);
 
-               $productresult_dt->update([
-                                'prdt_qty'=>$prdt_qty,
-                                'prdt_hpp'=>$prdt_hpp
-                                 ]);
+              }else{
+                  DB::rollBack();
+                  $data=['status'=>'gagal','data'=>'gagal'];
+                  return json_encode($data);
               }
 
-             }else{
+              
+              
 
+             }else{
+          
 
               $prdt_hpp= format::format($request->prdt_hpp[$i]);
               $prdt_qty= format::format($request->prdt_qty[$i]);
               $detailid=d_productresult_dt::where('prdt_productresult',$id)->max('prdt_detailid')+1;
               $comp=$request->comp[$i];
-              $position=$request->position[$i];
+              $position=$request->position[$i]; 
 
                d_productresult_dt::create([
                                 'prdt_productresult'=>$id,
@@ -227,11 +270,28 @@ class d_product_result extends Model
                                 'prdt_comp'=>$comp,
                                 'prdt_position'=>$position,
                                 'prdt_item'=>$request->prdt_item[$i],
-                                'prdt_qty'=>$prdt_qty,
+                                'prdt_qty'=>$prdt_qty,  
+                                'prdt_qty_sisa'=>$prdt_qty,
                                 'prdt_hpp'=>$prdt_hpp
                                  ]);
 
-                 mutasi::tambahmutasi($request->prdt_item[$i],$prdt_qty,$comp,$position,'Hasil Produksi','3',$id,'','',$prdt_hpp);
+$simpanMutasi=mutasi::tambahmutasi($request->prdt_item[$i],$prdt_qty,$comp,$position,'Hasil Produksi','3',$id,'','',$prdt_hpp);
+
+              if($simpanMutasi['true']){
+                    
+
+              }else{
+                  DB::rollBack();
+                  $data=['status'=>'gagal','data'=>'gagal'];
+                  return json_encode($data);
+              }
+
+
+
+
+
+
+
          }
 
 
@@ -244,20 +304,20 @@ class d_product_result extends Model
  }
 
  static function destroy($id){
-  return DB::transaction(function () use ($id) {
+  return DB::transaction(function () use ($id) {  
         $updateProductresult=
                           d_product_result::where('pr_id',$id);
         if($updateProductresult->first()->pr_status=='Y'){
             $data=['status'=>'gagal','data'=>'Data sudah digunakan'];
             return json_encode($data);
         }
-
-
+         
+        
           $hapus_product_dt=d_productresult_dt::where('prdt_productresult',$id)->get();
 
-
-  //Hapus Material
-        for ($h=0; $h <count($hapus_product_dt) ; $h++) {
+         
+  //Hapus Material          
+        for ($h=0; $h <count($hapus_product_dt) ; $h++) {           
                 $hapusItem=$hapus_product_dt[$h]->prdt_item;
 
                 $hapus=d_productresult_dt::where('prdt_productresult',$id)->where('prdt_item',$hapusItem);
@@ -265,10 +325,29 @@ class d_product_result extends Model
                 if(count($hapus->first())!=0){
                   $permintaan=$hapus->first()->prdt_qty;
 
-                  if($permintaan>0){
-                        if(mutasi::hapusMutasi($hapusItem,$permintaan,$comp=1,$position=1,$flag='Hasil Produksi',$sm_reff=$id)){
-                          $hapus->delete();
-                        }
+                  if($permintaan>0){   
+
+
+
+              $comp=$request->comp[$h];
+              $position=$request->position[$h];                                 
+
+      $simpanMutasi=mutasi::hapusMutasi($hapusItem,$permintaan,$comp,$position,$flag='Hasil Produksi',$sm_reff=$id);
+
+
+              if($simpanMutasi['true']){
+                $hapus->delete();
+
+              }else{
+                  DB::rollBack();
+                  $data=['status'=>'gagal','data'=>'gagal'];
+                  return json_encode($data);
+              }
+
+
+
+
+
                     }
                 }
           }
@@ -279,3 +358,5 @@ class d_product_result extends Model
   });
  }
   }
+
+  
