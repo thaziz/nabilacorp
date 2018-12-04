05fe452738919cb73981e1050aba57c0bca05f13
123mahmud <37867983+123mahmud@users.noreply.github.com>
2018-12-04 22:30:34 +0700

    route

--- app/Modules/Inventory/routes.php
+++ app/Modules/Inventory/routes.php
@@ -9,7 +9,7 @@ Route::group(['namespace' => 'App\Modules\Inventory\Controllers', 'middleware'=>
 	// Route::get('/inventory/pengirimanproduksi/hapus', 'pengirimanproduksiController@hapus');
 	// Route::get('/inventory/pengirimanproduksi/edit', 'pengirimanproduksiController@edit');
 	// Route::get('/inventory/pengirimanproduksi/update', 'pengirimanproduksiController@update');
-	//versi baru
+	//versi baru mahmud
 	Route::get('/inventory/pengirimanproduksi/pengirimanproduksi', 'PengambilanItemController@index');
 	Route::get('/produksi/suratjalan/create/delivery/{comp}', 'PengambilanItemController@tabelDelivery');
 	Route::get('/produksi/pengambilanitem/cari/tabel/{tgl1}/{tgl2}/{comp}', 'PengambilanItemController@tabelKirim');
