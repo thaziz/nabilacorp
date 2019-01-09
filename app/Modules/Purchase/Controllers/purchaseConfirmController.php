ab80521c6c682029a11c82403fc57e6616ede82b
thaziz <taziz704@gmail.com>
2019-01-10 05:53:56 +0700

    update

--- app/Modules/Purchase/Controllers/purchaseConfirmController.php
+++ app/Modules/Purchase/Controllers/purchaseConfirmController.php
@@ -441,7 +441,7 @@ public function getDataRencanaPembelian(Request $request)
   public function confirmOrderSubmit(Request $request)
   {
 
-// dd($request->all());
+dd($request->statusOrderConfirm);
     if ($request->statusOrderConfirm == 'WT') {        
         $dataHeader = DB::table('d_purchase_order')->where('po_id',$request->idOrder)->update([
           'po_status'=>'FN'
